7f8487277bbde0a980a5400a1e529fec703b0d02
Ingalls12 <wertyun91@gmail.com>
2022-02-03 18:29:10 -0600

    agruegue la funcionalidad de error

--- src/components/Cuadrado.js
+++ src/components/Cuadrado.js
@@ -2,31 +2,55 @@ import React,{useState} from 'react';
 import "../styles/Cuadrado-Style.css"
 export default function Cuadrado(props){
     let color = "#FF9136"
-    const[valor, setValor] =  useState("")
+    const[valor, setValor] =  useState("")  
     const[activo,setActivo] = useState(false)
     function handleChange(e){
-        
-        if(valor==="X"){
-            handleRigthChange(e)
+        e.target.value= props.value;
+        if(isCorrect(e)){
+            if(valor==="X"){
+                handleRigthChange(e)
+    
+            }else{
+                setActivo(prev=>!prev)
+                activo ? e.target.style.backgroundColor = "white":e.target.style.backgroundColor = color;
+                activo ? e.target.style.color = "black":e.target.style.color = "white";
+                activo ? e.target.value = props.numero:e.target.value= props.value;
+             
+                
+            }
         }else{
-            setActivo(prev=>!prev)
-            activo ? e.target.style.backgroundColor = "white":e.target.style.backgroundColor = color;
-            activo ? e.target.style.color = "black":e.target.style.color = "white";
+            e.target.style.backgroundColor = "red"
         }
         
+        
+    }
+    function isCorrect(e){
+        if(e.target.value==1){
+            return true
+        }else{
+            return false
+        }
+       
     }
     function handleRigthChange(e){
         e.preventDefault();
-        if(activo){
-            handleChange(e)
+        e.target.value= props.value
+        if(!isCorrect(e)){
+            if(activo){
+                handleChange(e)
+            }else{
+                valor===""?setValor("X"):setValor("");
+            }
         }else{
-            valor===""?setValor("X"):setValor("");
+            e.target.style.backgroundColor = color;
+      
         }
+        
         
        
     }
     return(
-        <div className='cuadrado' onClick={handleChange} onContextMenu={handleRigthChange} >
+        <div className='cuadrado' onClick={handleChange} onContextMenu={handleRigthChange} value={props.value} >
             <p>{valor}</p>
         </div>
     )
--- src/components/Plantilla.js
+++ src/components/Plantilla.js
@@ -3,6 +3,10 @@ import Cuadrado from "./Cuadrado";
 import "../styles/Plantilla-Style.css"
 export default function Plantilla(){
     const n = 100;
-    
-    return <div className="grilla">{[...Array(n)].map((e,i)=><Cuadrado key={i} numero ={i+1}/> )}</div>;
-}
+    const lista = [0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1];
+    return <div className="grilla">{lista.map((e,i)=>{
+        return(
+            <Cuadrado key={i} numero ={i+1} value = {e}/>
+        )
+        } )}</div>;
+    }
