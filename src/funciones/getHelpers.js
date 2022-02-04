f74f3462b0d913d74df783520829bb1d984f1b68
Ingalls12 <wertyun91@gmail.com>
2022-02-03 20:15:41 -0600

    agregue un archivo que calcula las ayudas del picross

---
+++ src/funciones/getHelpers.js
@@ -0,0 +1,44 @@
+function separacion(lista){
+    let contenido_x = []
+    for(x=0;x<lista.length;x=x+10){
+        let n_lista = lista.slice(x,x+10);
+        contenido_x.push(n_lista)
+    }
+    return contenido_x
+}
+function transpuesta(lista){
+    array = lista[0].map((col, i) => lista.map(row => row[i]));
+    return array;
+}
+function contador(lista){
+    let contenido_x = {};
+    let n = 1;
+    lista.forEach(element => {
+        contenido_x[n] = [];
+        let cuenta = 0;
+        while(element.length>0){
+            let valor = element.shift();
+            if(valor>0){
+                cuenta = cuenta + 1;
+            }else{
+                if(cuenta>0){
+                    contenido_x[n].push(cuenta);
+                }
+                cuenta=0;
+            }
+        }
+        if(cuenta>0){
+            contenido_x[n].push(cuenta);
+        }
+        n++;
+    });
+    return contenido_x;
+}
+
+let lista = [ 0, 1, 0, 1, 1, 1, 0, 1, 1, 0 , 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1];
+let respuesta = separacion(lista);
+let respuesta_transpuesta = transpuesta(respuesta);
+let helper_izq = contador(respuesta);
+let helper_top = contador(respuesta_transpuesta)
+console.log(helper_izq)
+console.log(helper_top)
