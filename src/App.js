e187c22f253d56e33e3274581cb6d0f8fdb14d26
Ingalls12 <wertyun91@gmail.com>
2022-02-04 19:14:53 -0600

    borre lo que no necesitaba y cree una carpeta con reducer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Grilla from "./components/Grilla";
-
+import {random_list} from "./funciones/getRandomArray";
 import "./styles/app.css"
 function App() {
+  const lista = random_list()
   return (
     <div className="App">
       <h1>Picross</h1>
       <div className="principal">
-        <Grilla/>
+        <Grilla lista = {lista}/>
       </div>
      
     </div>
--- src/components/Grilla.js
+++ src/components/Grilla.js
@@ -1,18 +1,18 @@
-import React,{useState, useContext} from "react";
+import React,{useState} from "react";
 import Plantilla from "./Plantilla";
 import HelperIzq from "./helpers/HelperIzq"
 import HelperTop from "./helpers/HelperTop";
-import {random_list} from "../funciones/getRandomArray"
 import "../styles/Grilla-Style.css"
 import { gameContext } from "./gameContext";
-export default function Grilla(){
+export default function Grilla(props){
     const[mostrar,setMostrar]= useState(true);
-    const [juego, setJuego] = useState("Hola");
-    function render(){
-        setMostrar(prev=>!prev)
-       
-    }
-    const lista = random_list();
+    const [juego, setJuego] = useState([]);
+    const lista = props.lista;
+
+
+
+
+
     return(
         <>
             <gameContext.Provider value={lista}>
@@ -22,7 +22,7 @@ export default function Grilla(){
                     <div className="helper_top"><HelperTop /></div>
                 </div>
             </gameContext.Provider>
-            <button onClick={render}>Borrar</button>
+            <button onClick={()=>{setMostrar(prev=>!prev)}}>Borrar</button>
         </>
     )
 }
