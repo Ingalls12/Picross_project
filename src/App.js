f8a57a3fd6520e5eed6ab8d07958878abb3e9d7e
Ingalls12 <wertyun91@gmail.com>
2022-02-04 23:15:18 -0600

    correccion de bugs

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import { useReducer } from "react";
+import { useReducer, useState } from "react";
 import Grilla from "./components/Grilla";
-import { listaReducer } from "./reducers/arrReducer";
+import { puntacionReducer } from "./reducers/puntuacionReducer";
 import "./styles/app.css";
 import Puntuacion from "./components/Puntuacion";
 function App() {
-  const [arreglo, arregloDispatcher] = useReducer(listaReducer,listaReducer())
-  const respuesta =()=>arregloDispatcher({type:"NEW"})
-
-  console.log(respuesta)
-  console.log(arreglo);
+  const [puntaje,setPunaje] = useReducer(puntacionReducer,puntacionReducer())
+  const [puntos,setPuntos] = useState(puntaje)
   return (
     <div className="App">
       <h1>Picross</h1>
       <div className="principal">
-        <Grilla lista = {arreglo.list}/>
+        <Grilla />
       </div>
-      <Puntuacion/>
+      <Puntuacion puntaje = {puntos}/>
     </div>
   );
 }
