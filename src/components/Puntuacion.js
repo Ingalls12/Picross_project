bba081c2d0bc658aba3ea52b14ad3034546b1e1f
Ingalls12 <wertyun91@gmail.com>
2022-02-05 21:06:28 -0600

    agregue que se muestre los corazones segun las vidas restantes

--- src/components/Puntuacion.js
+++ src/components/Puntuacion.js
@@ -1,18 +1,28 @@
-import React, { useContext,useState } from "react";
+import React, { useContext,useEffect,useState } from "react";
 import { poinstContext } from "./context/pointsContext";
 import "../styles/Puntuacion-Style.css";
 
 export default function Puntuacion(){
     
     const state = useContext(poinstContext);
+    const dispatch = state.setPuntaje;
     const valor = state.puntaje;
-    let lifes = new Array(3);
-    const vidas = lifes.forEach((e)=>{
-        console.log("entro")
+    const [corazones,setCorazones] = useState([1,1,1]);
+    console.log(state);
+    const vidas = corazones.map((e,i)=>{
         return(
-            <span><img className="corazon" src="/img/corazon.png"/></span>
+            <span key = {i}><img className="corazon" src="/img/corazon.png"/></span>
         )
-    })
+    });
+    useEffect(()=>{
+        if(valor.vidas===3&&valor.puntos===0){
+           setCorazones([1,1,1])
+        }else{
+            setCorazones(corazones.slice(-3,-1))
+        }
+       
+       
+    },[valor.vidas])
  
     return(
         <div className="puntuacion">
