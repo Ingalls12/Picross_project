8fe628b21894920126e3b2fd75eb8a01d6a91428
Ingalls12 <wertyun91@gmail.com>
2022-02-06 00:06:14 -0600

    Mejores los estilos

--- src/components/Grilla.js
+++ src/components/Grilla.js
@@ -31,6 +31,7 @@ export default function Grilla(){
     const puntosTotales = countSquares(array.list);
     
     useEffect(()=>{
+    
         if(puntos.puntos === puntosTotales && puntos.vidas>0){
             setGanaste(true)
             console.log("Ganaste")
@@ -41,16 +42,18 @@ export default function Grilla(){
     },[puntos.puntos,puntos.vidas])
     return(
         <>
-            <gameContext.Provider value={array.list}>
-                <div className="centro">
-                    <div className="helper_izq" ><HelperIzq /></div>
-                    <div className="grilla">{mostrar?<Plantilla key={"a"} />:<Plantilla key={"b"} />} </div>
-                    <div className="helper_top"><HelperTop /></div>
-                </div>
-            </gameContext.Provider>
-            <button className="btn_borrar" onClick={borrar}>Borrar</button>
-            <button className="btn_nuevo" onClick={new_Puzzle}>Nuevo Puzzle</button>
-            {ganaste ? <div className="ganaste_mensaje"> Felicidades haz ganado</div>:<></>}
+            <div className="juego">
+                <gameContext.Provider value={array.list}>
+                    <div className="centro">
+                        <div className="helper_izq" ><HelperIzq /></div>
+                        <div className="grilla">{mostrar?<Plantilla key={"a"} />:<Plantilla key={"b"} />} </div>
+                        <div className="helper_top"><HelperTop /></div>
+                    </div>
+                </gameContext.Provider>
+                <button className="btn_borrar" onClick={borrar}>Borrar</button>
+                <button className="btn_nuevo" onClick={new_Puzzle}>Nuevo Puzzle</button>
+            </div>
+            {ganaste ? <div className="ganaste_mensaje"> Felicidades haz ganado!!!</div>:<></>}
             {perdiste ? <div className="perdiste_mensaje"> Haz perdido </div>:<></>}
         </>
     )
